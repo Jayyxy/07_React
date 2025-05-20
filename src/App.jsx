c6f27a411eee0ea3199899f3894f8fcd927ddd58
Jayyxy <mimichoi0121@gmail.com>
2025-05-20 10:52:19 +0900

    import 해야함

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,25 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // 상태(state)
+  const [showExam, setShowExam] = useState();
 
   return (
+    //  <></> :fragment(html 역할 x)
+
+    /* 
+    jsx 주석
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hello world</h1>
+      <h1>Hello world</h1>
+    </>
+  */
+
+    <>
+      <button onClick={() => setShowExam(!showExam)}>클릭</button>
+
+      {/*showExam 이 true면 화면에 Exam1 컴포넌트 호출하여 렌더링함*/}
+      {showExam && <Exam2 jaei="helld" test="world" />}
     </>
   );
 }
